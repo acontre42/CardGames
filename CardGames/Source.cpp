f07db60b09610a0deaff96caf3adbb7d1ed6c810
acontre42 <152231739+acontre42@users.noreply.github.com>
2025-12-30 15:36:00 -0800

    Simplified main menu code in Source.cpp.

--- CardGames/Source.cpp
+++ CardGames/Source.cpp
@@ -9,36 +9,21 @@ using namespace std;
 
 int main()
 {
-	char choice = ' ';
-	bool validChoice = false;
 	BlackjackGame blackjack;
 	GoFish gofish;
+	char choice;
 
-	while (choice != 'q')
+	while (true)
 	{
-		choice = ' ';
-
-		while (!validChoice)
-		{
-			cout << "***** CARD GAMES *****" << endl;
-			cout << "What card game would you like to play? Enter an option or 'Q' to quit." << endl;
-			cout << "a) Blackjack" << endl;
-			cout << "b) Go Fish" << endl;
-			cout << "Enter your choice: ";
-			cin >> choice;
-			choice = tolower(choice);
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			cout << endl;
-			if (choice == 'a' || choice == 'b' || choice == 'q')
-			{
-				validChoice = true;
-			}
-			else
-			{
-				cout << "Invalid choice." << endl << endl;
-			}
-		}
-		validChoice = false;
+		cout << "***** CARD GAMES *****" << endl;
+		cout << "What card game would you like to play? Enter one of the options below or enter 'Q' to quit." << endl;
+		cout << "a) Blackjack" << endl;
+		cout << "b) Go Fish" << endl;
+		cout << "Enter your choice: ";
+		cin >> choice;
+		choice = tolower(choice);
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << endl;
 
 		switch (choice)
 		{
@@ -48,8 +33,11 @@ int main()
 		case 'b':
 			gofish.run();
 			break;
-		default:
+		case 'q':
 			cout << "Goodbye!" << endl;
+			exit(0);
+		default:
+			cout << "Invalid choice." << endl << endl;
 		}
 	}
 	
