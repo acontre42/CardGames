bbe5a350bf819298ef8e3ee14c46671cfbe54a7d
acontre42 <152231739+acontre42@users.noreply.github.com>
2025-12-30 14:56:30 -0800

    Moved function definitions from Card.h to Card.cpp.

--- CardGames/Card.cpp
+++ CardGames/Card.cpp
@@ -1,1 +1,72 @@
 #include "Card.h"
+
+Card::Card(int value, int symbol) 
+{
+	this->value = value;
+	this->symbol = symbol;
+	if (symbol >= CLUB && symbol <= SPADE)
+	{
+		color = BLACK;
+	}
+	else
+	{
+		color = RED;
+	}
+}
+
+Card::~Card()
+{
+	//cout << "DESTRUCTOR" << endl;
+}
+
+int Card::getColor()
+{
+	return color;
+}
+
+int Card::getSymbol()
+{
+	return symbol;
+}
+
+int Card::getValue()
+{
+	return value;
+}
+
+void Card::display()
+{
+	switch (value)
+	{
+	case J:
+		cout << "Jack";
+		break;
+	case Q:
+		cout << "Queen";
+		break;
+	case K:
+		cout << "King";
+		break;
+	case A:
+		cout << "Ace";
+		break;
+	default:
+		cout << value;
+	}
+	cout << " of ";
+	switch (symbol)
+	{
+	case CLUB:
+		cout << "Clubs";
+		break;
+	case SPADE:
+		cout << "Spades";
+		break;
+	case DIAMOND:
+		cout << "Diamonds";
+		break;
+	case HEART:
+		cout << "Hearts";
+		break;
+	}
+}
--- CardGames/Card.h
+++ CardGames/Card.h
@@ -12,74 +12,14 @@ public:
 	enum COLORS { BLACK, RED };
 	enum SYMBOLS { CLUB, SPADE, DIAMOND, HEART };
 	enum VALUES { A = 1, J = 11, Q = 12, K = 13 }; // In most cases, 'A' has value of 1.
-	Card(int value, int symbol)
-	{
-		this->value = value;
-		this->symbol = symbol;
-		if (symbol >= CLUB && symbol <= SPADE)
-		{
-			color = BLACK;
-		}
-		else
-		{
-			color = RED;
-		}
-		//cout << "CONSTRUCTOR" << endl;
-	}
-	~Card()
-	{
-		//cout << "DESTRUCTOR" << endl;
-	}
+	Card(int value, int symbol);
+	~Card();
 	//Getters
-	int getColor()
-	{
-		return color;
-	}
-	int getSymbol()
-	{
-		return symbol;
-	}
-	int getValue()
-	{
-		return value;
-	}
+	int getColor();
+	int getSymbol();
+	int getValue();
 	// Other Methods
 	// Display symbol and value of card in easy-to-read format.
-	void display()
-	{
-		switch (value)
-		{
-		case J:
-			cout << "Jack";
-			break;
-		case Q:
-			cout << "Queen";
-			break;
-		case K:
-			cout << "King";
-			break;
-		case A:
-			cout << "Ace";
-			break;
-		default:
-			cout << value;
-		}
-		cout << " of ";
-		switch (symbol)
-		{
-		case CLUB:
-			cout << "Clubs";
-			break;
-		case SPADE:
-			cout << "Spades";
-			break;
-		case DIAMOND:
-			cout << "Diamonds";
-			break;
-		case HEART:
-			cout << "Hearts";
-			break;
-		}
-	}
+	void display();
 };
 
