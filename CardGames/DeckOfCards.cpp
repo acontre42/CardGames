16821dff607700a33c2b6e58fc67b3b001e7a1c2
acontre42 <152231739+acontre42@users.noreply.github.com>
2025-12-31 11:57:07 -0800

    Moved function definitions from DeckOfCards.h to DeckOfCards.cpp.

--- CardGames/DeckOfCards.cpp
+++ CardGames/DeckOfCards.cpp
@@ -1,1 +1,166 @@
 #include "DeckOfCards.h"
+
+DeckOfCards::DeckOfCards(int numDecks)
+{
+	this->numDecks = numDecks;
+	if (numDecks > 0)
+	{
+		for (int i = 1; i <= numDecks; i++)
+		{
+			populate();
+		}
+		shuffle(deck.begin(), deck.end(), random_device());
+	}
+}
+
+int DeckOfCards::getNumDecks()
+{
+	return numDecks;
+}
+
+size_t DeckOfCards::getSize()
+{
+	return deck.size();
+}
+
+bool DeckOfCards::empty()
+{
+	return (deck.empty() ? true : false);
+}
+
+Card DeckOfCards::getCard()
+{
+	if (!deck.empty())
+	{
+		Card card = deck.back();
+		deck.pop_back();
+		return card;
+	}
+	else
+	{
+		cout << "Cannot get card from empty deck." << endl;
+		exit(1);
+	}
+}
+
+vector<Card> DeckOfCards::getCardsOfValue(int value)
+{
+	vector<Card> returnedCards;
+	if (contains(value))
+	{
+		while (contains(value))
+		{
+			for (int i = 0; i < deck.size(); i++)
+			{
+				if (deck[i].getValue() == value)
+				{
+					swap(deck[i], deck.back());
+					break;
+				}
+			}
+			returnedCards.push_back(deck.back());
+			deck.pop_back();
+		}
+	}
+	else
+	{
+		cout << "No cards in deck contain desired value." << endl;
+	}
+	return returnedCards;
+}
+
+int DeckOfCards::getValueAt(int index)
+{
+	if (index > deck.size())
+	{
+		cout << "ERROR: index greater than size of deck." << endl;
+		exit(1);
+	}
+	else
+	{
+		return deck.at(index).getValue();
+	}
+}
+
+void DeckOfCards::display()
+{
+	if (deck.empty())
+	{
+		cout << "N/A" << endl;
+	}
+	else
+	{
+		for (auto d : deck)
+		{
+			d.display();
+			cout << endl;
+		}
+	}
+}
+
+void DeckOfCards::last()
+{
+	Card card = deck.back();
+	card.display();
+}
+
+void DeckOfCards::peek()
+{
+	Card card = deck.front();
+	card.display();
+	cout << endl;
+}
+
+void DeckOfCards::populate()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 1; j <= 13; j++)
+		{
+			deck.emplace_back(j, i);
+		}
+	}
+}
+
+void DeckOfCards::reshuffle()
+{
+	if (numDecks > 0)
+	{
+		shuffle(deck.begin(), deck.end(), random_device());
+	}
+}
+
+void DeckOfCards::addCard(Card card)
+{
+	deck.push_back(card);
+}
+
+void DeckOfCards::addCard(int value, int symbol)
+{
+	deck.emplace_back(value, symbol);
+}
+
+void DeckOfCards::clear()
+{
+	if (deck.size() > 0)
+	{
+		deck.clear();
+	}
+}
+
+bool DeckOfCards::contains(int value)
+{
+	for (auto& card : deck)
+	{
+		if (card.getValue() == value)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void DeckOfCards::sortByAscValue()
+{
+	sort(deck.begin(), deck.end(), [](Card a, Card b) {return a.getValue() < b.getValue();});
+}
--- CardGames/DeckOfCards.h
+++ CardGames/DeckOfCards.h
@@ -10,176 +10,26 @@ private:
 	vector<Card> deck;
 	int numDecks; // How many packs of 52 cards in deck.
 public:	
-	DeckOfCards(int numDecks = 0)
-	{
-		this->numDecks = numDecks;
-		if (numDecks > 0)
-		{
-			for (int i = 1; i <= numDecks; i++)
-			{
-				populate();
-			}
-			shuffle(deck.begin(), deck.end(), random_device());
-		}
-	}
+	DeckOfCards(int numDecks = 0);
 	//Getters
-	int getNumDecks()
-	{
-		return numDecks;
-	}
-	size_t getSize() // is size_t ok?
-	{
-		return deck.size();
-	}
-	// Returns true if deck.empty() returns true.
-	bool empty()
-	{
-		if (deck.empty())
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	// Returns a Card from the deck.
-	Card getCard()
-	{
-		if (!deck.empty())
-		{
-			Card card = deck.back();
-			deck.pop_back();
-			return card;
-		}
-		else
-		{
-			cout << "Cannot get card from empty deck." << endl;
-			exit(1);
-		}
-	}
+	int getNumDecks();
+	size_t getSize(); // is size_t ok? 
+	bool empty(); // Returns true if deck.empty() returns true.
+	Card getCard(); // Returns a Card from the deck.
 	// Returns Cards of specific value from deck.
 	// Checks if deck has at least one card with that value. If yes, continuously swap it to back and pop it
 	// off into a returnedCards vector until all cards of that value have been removed.
-	vector<Card> getCardsOfValue(int value)
-	{
-		vector<Card> returnedCards;
-		if (contains(value))
-		{
-			while (contains(value))
-			{
-				for (int i = 0; i < deck.size(); i++)
-				{
-					if (deck[i].getValue() == value)
-					{
-						swap(deck[i], deck.back());
-						break;
-					}
-				}
-				returnedCards.push_back(deck.back());
-				deck.pop_back();
-			}
-		}
-		else
-		{
-			cout << "No cards in deck contain desired value." << endl;
-		}
-		return returnedCards;
-	}
-	// Returns value of Card at a particular index in deck.
-	int getValueAt(int index)
-	{
-		if (index > deck.size())
-		{
-			cout << "ERROR: index greater than size of deck." << endl;
-			exit(1);
-		}
-		else
-		{
-			return deck.at(index).getValue();
-		}
-	}
-	// Display all cards in deck.
-	void display()
-	{
-		if (deck.empty())
-		{
-			cout << "N/A" << endl;
-		}
-		else
-		{
-			for (auto d : deck)
-			{
-				d.display();
-				cout << endl;
-			}
-		}
-	}
-	// Display last card.
-	void last()
-	{
-		Card card = deck.back();
-		card.display();
-	}
-	// Display top card.
-	void peek()
-	{
-		Card card = deck.front();
-		card.display();
-		cout << endl;
-	}
-	// Populate the deck vector with the standard 52 cards in a deck of cards.
-	void populate()
-	{
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 1; j <= 13; j++)
-			{
-				deck.emplace_back(j, i);
-			}
-		}
-	}
-	// Shuffles deck if not empty.
-	void reshuffle()
-	{
-		if (numDecks > 0)
-		{
-			shuffle(deck.begin(), deck.end(), random_device());
-		}
-	}
-	// Add Card to deck.
-	void addCard(Card card)
-	{
-		deck.push_back(card);
-	}
-	void addCard(int value, int symbol)
-	{
-		deck.emplace_back(value, symbol);
-	}
-	// Empty deck vector.
-	void clear()
-	{
-		if (deck.size() > 0)
-		{
-			deck.clear();
-		}
-	}
-	// Returns true if value is found in deck.
-	bool contains(int value)
-	{
-		for (auto& card : deck)
-		{
-			if (card.getValue() == value)
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-	// Sorts deck vector in ascending order by value of Cards.
-	void sortByAscValue()
-	{
-		sort(deck.begin(), deck.end(), [](Card a, Card b) {return a.getValue() < b.getValue();});
-	}
+	vector<Card> getCardsOfValue(int value); 
+	int getValueAt(int index); // Returns value of Card at a particular index in deck.
+	void display(); // Display all cards in deck.
+	void last(); // Display last card.
+	void peek(); // Display top card.
+	void populate(); // Populate the deck vector with the standard 52 cards in a deck of cards.
+	void reshuffle(); // Shuffles deck if not empty.
+	void addCard(Card card); // Add Card to deck.
+	void addCard(int value, int symbol); // Create Card and add to deck. 
+	void clear(); // Empty deck vector.
+	bool contains(int value); // Returns true if value is found in deck.
+	void sortByAscValue(); // Sorts deck vector in ascending order by value of Cards.
 };
 
